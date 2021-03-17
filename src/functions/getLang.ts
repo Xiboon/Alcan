3f0e60232a9cb51a9505814eb6c7e12fdddfcc51
Xiboon <xibon@sex>
2021-03-17 22:08:43 +0100

    asdfadfasdfasdf
    asdfasdfasdfasdf

--- src/functions/getLang.ts
+++ src/functions/getLang.ts
@@ -2,7 +2,7 @@ import { Guild } from 'discord.js';
 module.exports = function (guild: Guild) {
 	let lang;
 	if (!guild.settings)
-		return new Error('xibon idioto czemu wsadzasz obiekt guild ktory nie ma settings!!!!');
+		return console.error('xibon idioto czemu wsadzasz obiekt guild ktory nie ma settings!!!!');
 	if (guild.settings.lang == 'pl') {
 		lang = require('../../translations/pl.json');
 	} else {
